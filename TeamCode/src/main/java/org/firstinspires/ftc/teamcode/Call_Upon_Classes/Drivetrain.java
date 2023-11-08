664d359e477afeda96cc0106eedd8eafe7ca65e8
Stephen O <stephenconochie@gmail.com>
2023-11-08 10:20:47 -0500

    Added Test Drivetrain Code
    - Thanks to Team 15 (Tommy, Noah, & Kevin), we now have final code for the drivetrain
    - This should work as it's much more cleaner to code
    - Will test on Thursday

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Call_Upon_Classes/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Call_Upon_Classes/Drivetrain.java
@@ -5,6 +5,7 @@ import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gam
 import androidx.annotation.NonNull;
 
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.hardware.RevIMU;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
@@ -34,6 +35,9 @@ public class Drivetrain {
     private DcMotor x_encoder = null;
     private MecanumDrive drive;
     private GamepadEx driverOp;
+
+    static final boolean FIELD_CENTRIC = false;
+    RevIMU imu = null;
 
 //
 //    /// new library stuff ///
@@ -81,22 +85,105 @@ public class Drivetrain {
         br.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
-    public void init_ftc_drive(HardwareMap hardwareMap, Gamepad gamepad1){
+    public void init_ftclib_drive(HardwareMap hardwareMap, Gamepad gamepad1){
+        // constructor takes in frontLeft, frontRight, backLeft, backRight motors
+        // IN THAT ORDER
         MecanumDrive drive = new MecanumDrive(
                 new Motor(hardwareMap, "fl", Motor.GoBILDA.RPM_435),
                 new Motor(hardwareMap, "fr", Motor.GoBILDA.RPM_435),
                 new Motor(hardwareMap, "bl", Motor.GoBILDA.RPM_435),
                 new Motor(hardwareMap, "br", Motor.GoBILDA.RPM_435)
         );
-        driverOp = new GamepadEx(gamepad1);
+
+        // This is the built-in IMU in the REV hub.
+        // We're initializing it by its default parameters
+        // and name in the config ('imu'). The orientation
+        // of the hub is important. Below is a model
+        // of the REV Hub and the orientation axes for the IMU.
+        //
+        //                           | Z axis
+        //                           |
+        //     (Motor Port Side)     |   / X axis
+        //                       ____|__/____
+        //          Y axis     / *   | /    /|   (IO Side)
+        //          _________ /______|/    //      I2C
+        //                   /___________ //     Digital
+        //                  |____________|/      Analog
+        //
+        //                 (Servo Port Side)
+        //
+        // (unapologetically stolen from the road-runner-quickstart)
+
+        RevIMU imu = new RevIMU(hardwareMap);
+        imu.init();
+
+        // the extended gamepad object
+
     }
 
-    public void run_ftc_drive(Gamepad gamepad1, Telemetry telemetry){
-        drive.driveRobotCentric(
-                driverOp.getLeftX(),
-                driverOp.getLeftY(),
-                driverOp.getRightY()
+    public void run_ftclib_drive(Gamepad gamepad, HardwareMap hardwareMap) {
+        MecanumDrive drive = new MecanumDrive(
+                new Motor(hardwareMap, "fl", Motor.GoBILDA.RPM_435),
+                new Motor(hardwareMap, "fr", Motor.GoBILDA.RPM_435),
+                new Motor(hardwareMap, "bl", Motor.GoBILDA.RPM_435),
+                new Motor(hardwareMap, "br", Motor.GoBILDA.RPM_435)
         );
+        // Driving the mecanum base takes 3 jo5ystick parameters: leftX, leftY, rightX.
+        // These are related to the left stick x value, left stick y value, and
+        // right stick x value respectively. These values are passed in to represent the
+        // strafing speed, the forward speed, and the turning speed of the robot frame
+        // respectively from [-1, 1].
+
+        if (!FIELD_CENTRIC) {
+
+            // For a robot centric model, the input of (0,1,0) for (leftX, leftY, rightX)
+            // will move the robot in the direction of its current heading. Every movement
+            // is relative to the frame of the robot itself.
+            //
+            //                 (0,1,0)
+            //                   /
+            //                  /
+            //           ______/_____
+            //          /           /
+            //         /           /
+            //        /___________/
+            //           ____________
+            //          /  (0,0,1)  /
+            //         /     ↻     /
+            //        /___________/
+
+            // optional fourth parameter for squared inputs
+            drive.driveRobotCentric(
+                    -gamepad.left_stick_x,
+                    gamepad.left_stick_y,
+                    -gamepad.right_stick_x,
+                    false);
+        } else {
+
+            // Below is a model for how field centric will drive when given the inputs
+            // for (leftX, leftY, rightX). As you can see, for (0,1,0), it will travel forward
+            // regardless of the heading. For (1,0,0) it will strafe right (ref to the 0 heading)
+            // regardless of the heading.
+            //
+            //                   heading
+            //                     /
+            //            (0,1,0) /
+            //               |   /
+            //               |  /
+            //            ___|_/_____
+            //          /           /
+            //         /           / ---------- (1,0,0)
+            //        /__________ /
+
+            // optional fifth parameter for squared inputs
+            drive.driveFieldCentric(
+                    gamepad.left_stick_x,
+                    gamepad.left_stick_y,
+                    gamepad.right_stick_x,
+                    imu.getRotation2d().getDegrees(),   // gyro value passed in here must be in degrees
+                    false
+            );
+        }
     }
 
     //Old method running Drivetrain
