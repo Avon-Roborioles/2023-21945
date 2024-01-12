5d3bb218c5512a9fa06b30eef0406d9d526b8fc4
Stephen O <stephenconochie@gmail.com>
2024-01-12 10:40:07 -0500

    Added Sudo Code for Score_Plus
    - I have added some sudo code for the logic of our auto program
    - The goal is to solely work on RR_ Score_Plus untill successful (hopefully 1 week before comp)
    - when RR_Score_Plus is finished, we quickly adjust for other positions and test (rapid progress)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Park_Score_Plus/RR_Score_Plus.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Park_Score_Plus/RR_Score_Plus.java
@@ -13,11 +13,29 @@ import java.util.concurrent.TimeUnit;
 //@Autonomous(name="RR Score", group="Park + Score")
 @Disabled
 public class RR_Score_Plus extends org.firstinspires.ftc.teamcode.Autonomous.AutoBase{
+
+    SampleMecanumDrive bot = new SampleMecanumDrive(hardwareMap);
+
+    TrajectorySequence quickStrafe = null;
+    double Xaxis = 0;
+    double Yaxis = 0;
+    double heading = Math.toRadians(0);
+    public void QuickStrafe(double distance){
+        quickStrafe = bot.trajectorySequenceBuilder(new Pose2d(Xaxis, Yaxis, heading))
+                .lineToLinearHeading(new Pose2d(Xaxis, Yaxis + distance, heading))
+                .build();
+        Yaxis += distance;
+    }
+
     public void runOpMode() throws InterruptedException {
-        String propPosition = "MIDDLE";
+
+
+        //important variables for auto - set to random values
+        String propPosition = "LEFT";
         int aprilTagID = 5;
-        init_classes();
-        SampleMecanumDrive bot = new SampleMecanumDrive(hardwareMap);
+
+        init_classes(); //init robot functions
+        //SampleMecanumDrive bot = new SampleMecanumDrive(hardwareMap);
 
         TrajectorySequence LeftSpikeScore = bot.trajectorySequenceBuilder(new Pose2d()) //TODO
                 .build();
@@ -28,29 +46,40 @@ public class RR_Score_Plus extends org.firstinspires.ftc.teamcode.Autonomous.Aut
         TrajectorySequence RightSpikeScore = bot.trajectorySequenceBuilder(new Pose2d()) //TODO
                 .build();
 
-        TrajectorySequence PreloadScore = bot.trajectorySequenceBuilder(new Pose2d()) //TODO
+        TrajectorySequence PreloadScore1 = bot.trajectorySequenceBuilder(new Pose2d()) //TODO
+                .build();
+        TrajectorySequence PreloadScore2 = bot.trajectorySequenceBuilder(new Pose2d()) //TODO
+                .build();
+        TrajectorySequence PreloadScore3 = bot.trajectorySequenceBuilder(new Pose2d()) //TODO
                 .build();
 
-        TrajectorySequence getStackedPixel = bot.trajectorySequenceBuilder(new Pose2d())//TODO
-                .build();
-
-        TrajectorySequence scorePixel = bot.trajectorySequenceBuilder(new Pose2d())
-                .build();
-
-        TrajectorySequence park = bot.trajectorySequenceBuilder(new Pose2d()) //TODO
-                .build();
+//        //safe spot for scoring region
+//        TrajectorySequence SafeSpot1 = bot.trajectorySequenceBuilder(new Pose2d()) //TODO
+//                .build();
+//
+//        //safe spot for retrieving region
+//        TrajectorySequence SafeSpot2 = bot.trajectorySequenceBuilder(new Pose2d()) //TODO
+//                .build();
+//
+//
+//        TrajectorySequence get2StackedPixels = bot.trajectorySequenceBuilder(new Pose2d())//TODO
+//                .build();
+//
+//        TrajectorySequence scorePixels = bot.trajectorySequenceBuilder(new Pose2d()) //TODO
+//                .build();
+//
+//        TrajectorySequence park = bot.trajectorySequenceBuilder(new Pose2d()) //TODO
+//                .build();
 
         //auto code here
         waitForStart();
 
         //variables stored after 3 seconds of camera processing
-        Timing.Timer clock = new Timing.Timer(3, TimeUnit.SECONDS);
-        while(clock.isTimerOn()) {
-            propPosition = vision.getPropPosition();
-            aprilTagID = vision.get_Apriltag_id(propPosition, false);
-        }
+        propPosition = vision.getPropPosition(); //get latest prop position
+        aprilTagID = vision.get_Apriltag_id(propPosition, false);
 
-        //scores the purple preload pixel based on vison reading
+
+        //scores purple pixel on spike based on vision reading
         switch(propPosition){
             case "LEFT":
                 bot.followTrajectorySequence(LeftSpikeScore);
@@ -63,11 +92,63 @@ public class RR_Score_Plus extends org.firstinspires.ftc.teamcode.Autonomous.Aut
                 break;
         }
 
-        //score pixel
-        bot.followTrajectorySequence(PreloadScore);
-
-        //park robot
-        bot.followTrajectorySequence(park);
+        //score yellow pixel on board based on vision reading
+        switch (aprilTagID) {
+            case 4:
+                bot.followTrajectorySequence(PreloadScore1);
+                break;
+            case 5:
+                bot.followTrajectorySequence(PreloadScore2);
+                break;
+            case 6:
+                bot.followTrajectorySequence(PreloadScore3);
+        }
+//
+//        //getting ready to detect stacked pixels
+//        //vision.init_stack_detection(hardwareMap);
+//
+//        //go to scoring safe spot
+//        bot.followTrajectorySequence(SafeSpot1);
+//
+//        //go to retrieving safe spot
+//        bot.followTrajectorySequence(SafeSpot2);
+//
+//        //strafes to 1 of 2 available stacks
+////        double strafeDistance = vision.getStrafeDistance();
+////        QuickStrafe(strafeDistance);
+//
+//        bot.followTrajectorySequence(get2StackedPixels);
+//
+//        bot.followTrajectorySequence(SafeSpot2);
+//
+//        bot.followTrajectorySequence(SafeSpot1);
+//
+//        //TODO check for board availablility (able to find at least 2 tags?)
+//
+//        bot.followTrajectorySequence(scorePixels);
+//
+//        bot.followTrajectorySequence(SafeSpot1);
+//
+//        bot.followTrajectorySequence(SafeSpot2);
+//
+//        //strafes to 1 of 2 available stacks
+////        double strafeDistance = vision.getStrafeDistance();
+////        QuickStrafe(strafeDistance);
+//
+//        bot.followTrajectorySequence(get2StackedPixels);
+//
+//        bot.followTrajectorySequence(SafeSpot2);
+//
+//        bot.followTrajectorySequence(SafeSpot1);
+//
+//        //TODO check for board availablility (able to find at least 2 tags?)
+//
+//        bot.followTrajectorySequence(scorePixels);
+//
+//        bot.followTrajectorySequence(SafeSpot1);
+//
+//        //park robot
+//        bot.followTrajectorySequence(park);
 
 
 
