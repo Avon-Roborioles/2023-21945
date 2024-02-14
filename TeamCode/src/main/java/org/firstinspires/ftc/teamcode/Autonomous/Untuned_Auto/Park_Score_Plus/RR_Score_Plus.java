0637cfc0a12c0961c9e4e2c55b9fd2484692b634
Stephen O <stephenconochie@gmail.com>
2024-02-14 09:14:20 -0500

    Added Finite State Machine Logic into RR_Score_Plus Program
    - i am hopeful this will work first try
    - The Finite State Machine logic should allow us to run our normal program logic while controlling other processes like PID control telemetry, etc

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Untuned_Auto/Park_Score_Plus/RR_Score_Plus.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Untuned_Auto/Park_Score_Plus/RR_Score_Plus.java
@@ -11,6 +11,24 @@ import java.util.concurrent.atomic.AtomicReference;
 
 @Autonomous(name="RR Score Plus", group="Park + Score")
 public class RR_Score_Plus extends org.firstinspires.ftc.teamcode.Autonomous.AutoBase{
+
+    //this enum defines our state
+    enum State {
+        LEFT_SPIKE_SCORE,
+        MIDDLE_SPIKE_SCORE,
+        RIGHT_SPIKE_SCORE,
+        LEFT_BOARD_SCORE,
+        MIDDLE_BOARD_SCORE,
+        RIGHT_BOARD_SCORE,
+        PARK,
+        WAIT,
+        IDLE
+    }
+
+    //define current state the robot is on
+    //default to IDLE
+    State currentState = State.IDLE;
+
     public void runOpMode() throws InterruptedException {
 
         //important variables for auto - set to random values
@@ -129,7 +147,6 @@ public class RR_Score_Plus extends org.firstinspires.ftc.teamcode.Autonomous.Aut
 //                .strafeLeft(21)
 //                .waitSeconds(.1)
 //                .back(15)
-
                 .build();
 
         //****************Board Score Trajectories include parking********************
@@ -138,55 +155,87 @@ public class RR_Score_Plus extends org.firstinspires.ftc.teamcode.Autonomous.Aut
         TrajectorySequence LeftBoardScore = bot.trajectorySequenceBuilder(LeftSpikeScore.end())
                 .lineToLinearHeading(new Pose2d(PoseStorage.LeftBoardPoseR.getX()-20,PoseStorage.LeftBoardPoseR.getY(),PoseStorage.LeftBoardPoseR.getHeading()))
                 .addDisplacementMarker(()->{
-                    state.set("Score Yellow Pixel on Left Board Region");
+                    //state.set("Score Yellow Pixel on Left Board Region");
                     arm.up();
                     intake.wrist_up();
                     intake.closeClaws(true);
                 })
-//                .waitSeconds(.7)
-//                .back(5)
-//                .addDisplacementMarker(()->{
-//                    intake.openClawV2(true,true);
-//                })
-//                .waitSeconds(.7)
-//                .forward(5)
-//                .addDisplacementMarker(()->{
-//                    state.set("Park");
-//                    arm.down();
-//                    intake.closeClaws(true);
-//                })
-//                .waitSeconds(.7)
-//                .lineToLinearHeading(new Pose2d(PoseStorage.ParkSpotRR.getX()-20,PoseStorage.ParkSpotRR.getY(),PoseStorage.ParkSpotRR.getHeading()))
-//                .waitSeconds(.1)
-//                .lineToLinearHeading(PoseStorage.ParkSpotRR)
+                .waitSeconds(.7)
+                .back(5)
+                .addDisplacementMarker(()->{
+                    intake.openClawV2(true,true);
+                })
+                .waitSeconds(.7)
+                .forward(5)
+                .addDisplacementMarker(()->{
+                    state.set("Park");
+                    arm.down();
+                    intake.closeClaws(true);
+                })
+                .waitSeconds(.7)
+                .lineToLinearHeading(new Pose2d(PoseStorage.ParkSpotRR.getX()-20,PoseStorage.ParkSpotRR.getY(),PoseStorage.ParkSpotRR.getHeading()))
+                .waitSeconds(.1)
+                .lineToLinearHeading(PoseStorage.ParkSpotRR)
                 .build();
 
         //TODO
         TrajectorySequence MiddleBoardScore = bot.trajectorySequenceBuilder(MiddleSpikeScore.end())
                 .lineToLinearHeading(new Pose2d(PoseStorage.MiddleBoardPoseR.getX()-20,PoseStorage.MiddleBoardPoseR.getY(),PoseStorage.MiddleBoardPoseR.getHeading()))
                 .addDisplacementMarker(()->{
-                    state.set("Score Yellow Pixel on Middle Board Region");
+                    //state.set("Score Yellow Pixel on Middle Board Region");
                     arm.up();
                     intake.wrist_up();
                     intake.closeClaws(true);
                 })
+                .waitSeconds(.7)
+                .back(5)
+                .addDisplacementMarker(()->{
+                    intake.openClawV2(true,true);
+                })
+                .waitSeconds(.7)
+                .forward(5)
+                .addDisplacementMarker(()->{
+                    state.set("Park");
+                    arm.down();
+                    intake.closeClaws(true);
+                })
+                .waitSeconds(.7)
+                .lineToLinearHeading(new Pose2d(PoseStorage.ParkSpotRR.getX()-20,PoseStorage.ParkSpotRR.getY(),PoseStorage.ParkSpotRR.getHeading()))
                 .waitSeconds(.1)
+                .lineToLinearHeading(PoseStorage.ParkSpotRR)
                 .build();
 
         //TODO
         TrajectorySequence RightBoardScore = bot.trajectorySequenceBuilder(RightSpikeScore.end())
                 .lineToLinearHeading(new Pose2d(PoseStorage.RightBoardPoseR.getX()-20,PoseStorage.RightBoardPoseR.getY(),PoseStorage.RightBoardPoseR.getHeading()))
                 .addDisplacementMarker(()->{
-                    state.set("Score Yellow Pixel on Board");
+                    //state.set("Score Yellow Pixel on Board");
                     arm.up();
                     intake.wrist_up();
                     intake.closeClaws(true);
                 })
+                .waitSeconds(.7)
+                .back(5)
+                .addDisplacementMarker(()->{
+                    intake.openClawV2(true,true);
+                })
+                .waitSeconds(.7)
+                .forward(5)
+                .addDisplacementMarker(()->{
+                    state.set("Park");
+                    arm.down();
+                    intake.closeClaws(true);
+                })
+                .waitSeconds(.7)
+                .lineToLinearHeading(new Pose2d(PoseStorage.ParkSpotRR.getX()-20,PoseStorage.ParkSpotRR.getY(),PoseStorage.ParkSpotRR.getHeading()))
                 .waitSeconds(.1)
+                .lineToLinearHeading(PoseStorage.ParkSpotRR)
                 .build();
 
         //auto code here
         waitForStart();
+
+        if(isStopRequested()) return;
 
         //gets propPosition and needed april tag from vision class
         propPosition = vision.getPropPosition();
@@ -195,14 +244,17 @@ public class RR_Score_Plus extends org.firstinspires.ftc.teamcode.Autonomous.Aut
         //scores the purple preload pixel based on vision reading
         switch(propPosition){
             case "LEFT":
+                currentState = State.LEFT_SPIKE_SCORE;
                 bot.followTrajectorySequenceAsync(LeftSpikeScore);
-                bot.followTrajectorySequenceAsync(LeftBoardScore);
                 break;
             case "MIDDLE":
+               currentState = State.MIDDLE_SPIKE_SCORE;
                 bot.followTrajectorySequenceAsync(MiddleSpikeScore);
                 break;
             case "RIGHT":
+                currentState = State.RIGHT_SPIKE_SCORE;
                 bot.followTrajectorySequenceAsync(RightSpikeScore);
+
                 break;
         }
 
@@ -219,12 +271,71 @@ public class RR_Score_Plus extends org.firstinspires.ftc.teamcode.Autonomous.Aut
 //                break;
 //        }
 
-        while(opModeIsActive()){
-            bot.update(); //handles RR logic
+        //init loop so we see if robot will start with correct propPosition
+        while(opModeInInit()){
+            telemetry.addData("Detected Prop Position: ", propPosition);
+            telemetry.addData("Corresponding April Tag:",aprilTagID);        }
+
+        while(opModeIsActive() && !isStopRequested()){
+            //define the flow of the state machine through this switch statement
+            switch (currentState){
+                case LEFT_SPIKE_SCORE:
+                    // Check if the drive class isn't busy
+                    // `isBusy() == true` while it's following the trajectory
+                    // Once `isBusy() == false`, the trajectory follower signals that it is finished
+                    // We move on to the next state
+                    // Make sure we use the async follow function
+                    if(!bot.isBusy()){
+                        currentState = State.LEFT_BOARD_SCORE;
+                        bot.followTrajectorySequenceAsync(LeftBoardScore);
+                    }
+                    break;
+                case MIDDLE_SPIKE_SCORE:
+                    if(!bot.isBusy()){
+                        currentState = State.MIDDLE_BOARD_SCORE;
+                        bot.followTrajectorySequenceAsync(MiddleBoardScore);
+                    }
+                    break;
+                case RIGHT_SPIKE_SCORE:
+                    if(!bot.isBusy()){
+                        currentState = State.RIGHT_BOARD_SCORE;
+                        bot.followTrajectorySequenceAsync(RightBoardScore);
+                    }
+                    break;
+                case LEFT_BOARD_SCORE:
+                case MIDDLE_BOARD_SCORE:
+                case RIGHT_BOARD_SCORE:
+                    if(!bot.isBusy()){
+                        currentState = State.IDLE;
+                    }
+                    break;
+                case IDLE:
+                    //Do nothing in IDLE except save position
+                    //current state does not change once in IDLE
+                    //This concludes the autonomous program
+                    break;
+            }
+
+            //anything outside the switch statement will run independent of the currentState
+
+            //we update drive continuously in the background regardless of state
+            bot.update();
+
+            //read pose
+            Pose2d poseEstimate = bot.getPoseEstimate();
+
+            //continuously write pose to 'PoseStorage'
+            PoseStorage.currentPose = poseEstimate;
+
             arm.update(); //handles Arm PID control
+
+            //telemetry data
             telemetry.addData("Detected Prop Position: ", propPosition);
             telemetry.addData("Corresponding April Tag:",aprilTagID);
-            telemetry.addData("Current Objective: ",state);
+            telemetry.addData("Current Objective: ",currentState);
+            telemetry.addData("X: ",poseEstimate.getX());
+            telemetry.addData("Y: ",poseEstimate.getY());
+            telemetry.addData("Heading: ",poseEstimate.getHeading());
             telemetry.update();
         }
     }
