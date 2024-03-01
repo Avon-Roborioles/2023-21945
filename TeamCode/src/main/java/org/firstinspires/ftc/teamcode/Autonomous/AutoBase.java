01e77a9b2300a799444b94f5efd84a43f6bbae3c
Stephen O <stephenconochie@gmail.com>
2024-03-01 10:45:09 -0500

    Implemented Auto Menu + FSM Code in Blue Right Auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutoBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutoBase.java
@@ -1,14 +1,42 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
 //Added Default Class for Basic Auto Functions (Used throughout all Autonomous Programs
 
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.ToggleButtonReader;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.Autonomous.Untuned_Auto.Park_Score_Plus.RR_Score_Plus;
 import org.firstinspires.ftc.teamcode.Call_Upon_Classes.*;
+import org.firstinspires.ftc.teamcode.Tests.AutoMenu;
 
 public class AutoBase extends LinearOpMode {
     protected org.firstinspires.ftc.teamcode.Call_Upon_Classes.Arm arm = new Arm();
     protected org.firstinspires.ftc.teamcode.Call_Upon_Classes.Intake intake = new Intake();
     protected org.firstinspires.ftc.teamcode.Call_Upon_Classes.Camera_Vision vision = new Camera_Vision();
 
+    public enum AutoPath {
+        UP,
+        DOWN
+    }
+
+    //this enum defines our state
+    public enum State {
+        LEFT_SPIKE_SCORE,
+        MIDDLE_SPIKE_SCORE,
+        RIGHT_SPIKE_SCORE,
+        LEFT_BOARD_SCORE,
+        MIDDLE_BOARD_SCORE,
+        RIGHT_BOARD_SCORE,
+        PARK,
+        WAIT,
+        IDLE
+    }
+
+    //define current state the robot is on
+    public State currentState = State.IDLE;
+
+    AutoPath selectedPath = AutoPath.UP;
+    public boolean BoardScore = true;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -19,6 +47,27 @@ public class AutoBase extends LinearOpMode {
         arm.init_arm_auto(hardwareMap,"leftMotor", "rightMotor");
         intake.init_intake_V2(hardwareMap, "claw1", "claw2","wrist");
         vision.init_cameras(hardwareMap, "Webcam 1", "Webcam 2");
+    }
+
+    public void runAutoMenu(GamepadEx gamepad, ToggleButtonReader d_up, ToggleButtonReader d_down){
+        if(d_up.getState()){ //Auto Path selection
+            selectedPath = AutoPath.UP;
+        } else {
+            selectedPath = AutoPath.DOWN;
+        }
+
+        BoardScore = d_down.getState(); //Board Score Selection
+
+        //menu
+        telemetry.addLine("Select the Auto Path by Toggling the D-pad Up Button");
+        telemetry.addData("Current Auto Path Selected: ", selectedPath);
+        telemetry.addLine(" ");
+        telemetry.addLine("Select Board Scoring by Toggling the D-pad Down Button");
+        telemetry.addData("Selected Board Score Status: ", BoardScore);
+
+        //keep checking for driver input
+        d_up.readValue();
+        d_down.readValue();
     }
 
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Tuned_Auto/Blue_Right_Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Tuned_Auto/Blue_Right_Auto.java
@@ -1,9 +1,13 @@
 package org.firstinspires.ftc.teamcode.Autonomous.Tuned_Auto;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.arcrobotics.ftclib.gamepad.ToggleButtonReader;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 
 import org.firstinspires.ftc.teamcode.Autonomous.AutoBase;
+import org.firstinspires.ftc.teamcode.Autonomous.Untuned_Auto.Park_Score_Plus.RR_Score_Plus;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 
@@ -13,6 +17,18 @@ public class Blue_Right_Auto extends AutoBase {
     public static Pose2d startPoseBR = new Pose2d(11.7,60,Math.toRadians(-90));
 
     public void runOpMode() throws  InterruptedException{
+        GamepadEx gamepad = new GamepadEx(gamepad1);
+
+        //auto menu code
+        ToggleButtonReader d_up = new ToggleButtonReader(
+                gamepad, GamepadKeys.Button.DPAD_UP
+        );
+        ToggleButtonReader d_down = new ToggleButtonReader(
+                gamepad, GamepadKeys.Button.DPAD_DOWN
+        );
+        runAutoMenu(gamepad,d_up,d_down);
+
+
         SampleMecanumDrive bot = new SampleMecanumDrive(hardwareMap);
 
         //important variables for auto - set to random values
@@ -23,13 +39,13 @@ public class Blue_Right_Auto extends AutoBase {
         vision.init_prop_detection(hardwareMap, true); //sets camera to start looking for prop
 
         //TODO
-        TrajectorySequence Left_Spike_Score = bot.trajectorySequenceBuilder(startPoseBR)
+        TrajectorySequence LeftSpikeScore = bot.trajectorySequenceBuilder(startPoseBR)
                 .build();
         //TODO
-        TrajectorySequence Middle_Spike_Score = bot.trajectorySequenceBuilder(startPoseBR)
+        TrajectorySequence MiddleSpikeScore = bot.trajectorySequenceBuilder(startPoseBR)
                 .build();
         //TODO
-        TrajectorySequence Right_Spike_Score = bot.trajectorySequenceBuilder(startPoseBR)
+        TrajectorySequence RightSpikeScore = bot.trajectorySequenceBuilder(startPoseBR)
                 .build();
 
         //TODO
@@ -57,5 +73,35 @@ public class Blue_Right_Auto extends AutoBase {
         //TODO
         TrajectorySequence Park = bot.trajectorySequenceBuilder(bot.getPoseEstimate())
                 .build();
+
+        //gets propPosition and needed april tag from vision class
+        propPosition = vision.getPropPosition();
+        aprilTagID = vision.get_Apriltag_id(propPosition,true);
+
+        telemetry.addData("Detected Prop Position: ", propPosition);
+        telemetry.addData("Required April Tag: ", aprilTagID);
+        telemetry.update();
+
+        waitForStart(); // loops through code above
+
+        if(isStopRequested()) return;
+
+        //scores the purple preload pixel based on vision reading
+        switch(propPosition){
+            case "LEFT":
+                currentState = State.LEFT_SPIKE_SCORE;
+                bot.followTrajectorySequenceAsync(LeftSpikeScore);
+                break;
+            case "MIDDLE":
+                currentState = State.MIDDLE_SPIKE_SCORE;
+                bot.followTrajectorySequenceAsync(MiddleSpikeScore);
+                break;
+            case "RIGHT":
+                currentState = State.RIGHT_SPIKE_SCORE;
+                bot.followTrajectorySequenceAsync(RightSpikeScore);
+                break;
+        }
+
+
     }
 }
